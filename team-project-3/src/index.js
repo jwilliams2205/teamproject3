d6842fcd4ad7cb252cac2f468496ac6cd82475a8
jwilliams2205 <47389051+jwilliams2205@users.noreply.github.com>
2021-04-16 16:36:52 -0400

    added lots of functionality. still needs a button to get it all going

--- team-project-3/src/index.js
+++ team-project-3/src/index.js
@@ -2,15 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Box extends React.Component{
-  selectBox = () =>{
-    this.props.selectBox(this.props.row, this.props.col);
+class Cell extends React.Component{
+  selectCell = () =>{
+    this.props.selectCell(this.props.row, this.props.col);
   }
 
   render(){
     return(
-      <div className = {this.props.boxClass} id = {this.props.boxId}
-      onClick = {this.selectBox} />
+      <div className = {this.props.cellClass} id = {this.props.cellId}
+      onClick = {this.selectCell} />
     )
   }
 }
@@ -20,21 +20,26 @@ class Grid extends React.Component{
 
     var rowArray = [];
 
-    var boxClass = "";
+    var cellClass = "";
 
     for(var i = 0; i < 20; i++){
       for(var j = 0; j < 20; j++){
-        var boxId =  i + "_" + j;
+        var cellId =  i + "_" + j;
 
-        boxClass = this.props.stateGrid[i][j] ? "box alive" : "box dead";
+        if(this.props.stateGrid[i][j]){
+          cellClass = "cell alive";
+        }
+        else{
+          cellClass = "cell dead";
+        }
         rowArray.push(
-          <Box
-            boxClass = {boxClass}
-            key = {boxId}
-            boxId = {boxId}
+          <Cell
+            cellClass = {cellClass}
+            key = {cellId}
+            cellId = {cellId}
             row = {i}
             col = {j}
-            selectBox = {this.props.selectBox}
+            selectCell = {this.props.selectCell}
             />
         )
       }
@@ -63,7 +68,24 @@ class Game extends React.Component{
   }
 }
 
-  selectBox = (i, j) =>{
+  randomize = () =>{
+    var randomTemp = this.state.stateGrid.map(array => array.slice());
+    for(var i = 0; i < 100; i++){
+      var iRand = Math.floor(Math.random()*20); //Create random i and j index to turn on for creating the seed.
+      var jRand = Math.floor(Math.random()*20);
+      if(randomTemp[iRand][jRand]){
+        i--;
+      }
+      else{
+        randomTemp[iRand][jRand] = true;
+      }
+    }
+    this.setState({
+      stateGrid: randomTemp
+    })
+  }
+
+  selectCell = (i, j) =>{
     var temp = this.state.stateGrid.map(array => array.slice());
     temp[i][j] = !temp[i][j];
     console.log(temp[i][j]);
@@ -72,11 +94,48 @@ class Game extends React.Component{
     })
   }
 
+  oneGen = () =>{
+    var permutations = [
+      [0,1], [0,-1], [1,0], [-1,0], [1,1], [-1,1], [-1,-1], [1,-1]
+    ]
+    var gameGrid = this.state.stateGrid.map(array => array.slice());
+    for(var i = 0; i < 20; i++){
+      for(var j = 0; j < 20; j++){
+        var popCount = 0;
+        //Rather than create a long list of if conditions, it seemed better to create an array of different neighbor permutations.
+        permutations.forEach(([iBox, jBox]) =>{
+          if(i+iBox >= 0 && i+iBox < 20 && j+jBox >= 0 && j+jBox < 20){
+            if(gameGrid[i+iBox][j+jBox] === true){
+              popCount++;
+            }
+          }
+        });
+        if(popCount === 3 || popCount === 2){ //If popCount is in the inferval (2,3) => Set the box to true. 
+          gameGrid[i][j] = true;
+        }
+      }
+    }
+    this.setState({
+      stateGrid: gameGrid
+    });
+  }
 
+  playIterations = (numIterations, infiniteFlag) => {
+    while(infiniteFlag){
+      this.oneGen();
+    }
+    for(var i = numIterations; i > 0; i--){
+      this.oneGen();
+    }
+  }
+
+  componentDidMount(){
+    this.randomize();
+  }
   render(){
     return(
       <div><header>Conway's Game of Life</header>
-      <Grid stateGrid = {this.state.stateGrid} selectBox = {this.selectBox}
+      <Grid stateGrid = {this.state.stateGrid} selectCell = {this.selectCell}
       />
       </div>
     )
